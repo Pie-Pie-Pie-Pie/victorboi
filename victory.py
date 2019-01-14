25682b78fcadadbeae69a3ca3a333d07fe5cf27c
Pie-Pie-Pie-Pie <46689516+Pie-Pie-Pie-Pie@users.noreply.github.com>
2019-01-14 12:31:13 -0800

    Create victory.py

---
+++ victory.py
@@ -0,0 +1,26 @@
+#globalvariables
+param1 = 0
+param2 = 0
+op = "+"
+
+#function definition
+def intro():
+    print("Welcome i am a robot built to assist you in MATH")
+    return
+
+def inputBoi():
+    #input first peram
+    param1=input("WHAT IS YOUR FIRST NUMBER")
+    param2=input("WHAT IS YOUR SECOND NUMBER")
+    op=input("WHAT DO YOU WANT ME TO DO +,-,*,/")
+    return
+    
+def calculate():
+    
+    return
+    
+#executable code
+intro()
+inputBoi()
+calculate()
+
